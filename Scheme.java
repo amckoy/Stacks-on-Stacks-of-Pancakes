bf0af733a498c57aa37d6c168999ab5ad0e99ab4
amckoy <amckoy@stuy.edu>
2016-04-01 10:40:53 -0400

    add Scheme to the party

---
+++ Scheme.java
@@ -0,0 +1,15 @@
+/***************************************************
+ * class Scheme
+ * A rudimentary Scheme interpreter
+ *
+ *ALGORITHM for EVALUATING A SCHEME EXPRESSION:
+      1. 
+ *
+ * STACK OF CHOICE: ALStack by Ayman Ahmed
+ * b/c ALStack doesn't need to traverse the entire list to reach an\
+ * index, and speed is maximized
+ ***************************************************/
+public class Scheme{
+    
+    public static int add(int a, int b){
+	
